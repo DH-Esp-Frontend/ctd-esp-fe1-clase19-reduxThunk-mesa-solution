0ca63f1afc1e697ca7b943ad455050c003acb2c8
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-01-31 10:44:16 -0300

    async function

--- src/thunk/Middleware.ts
+++ src/thunk/Middleware.ts
@@ -1,3 +1,27 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const thunkGetPokemons = createAsyncThunk("pokemons/",)
+export const thunkGetPokemons = createAsyncThunk("pokemons/",async ()=>{
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+  const data = await response.json();
+  return data.results;
+})
+
+export const thunkSearchPokemon = createAsyncThunk("pokemons",async (pokemonName)=>{
+      const data =  thunkGetPokemons();
+      const result = await filterFunction(data,pokemonName)
+    return result
+})
+
+const filterFunction =(data:any,pokemonName:any)=>{
+  return data.filter((pokemon:any) =>
+        pokemon.name.toLowerCase().startsWith(pokemonName.toLowerCase())
+      );
+}
+
+export const thunkGetAnPokemon = createAsyncThunk("pokemons",async(pokemonName)=>{
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`
+  );
+  return await response.json();
+})
+
